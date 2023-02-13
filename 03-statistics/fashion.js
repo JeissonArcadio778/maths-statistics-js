6a8990e4ab56d16d5d5492838ecce2e13c930715
jeissonarcadio778 <jeissonbeltranporras@gmail.com>
2023-02-12 20:05:55 -0500

    50% challenge

--- 03-statistics/fashion.js
+++ 03-statistics/fashion.js
@@ -1,23 +1,40 @@
 // Calculate the fashion
 
+/*objTimes {
+
+    "1" : 2,
+    "2" : 5
+
+}
+
+*/ 
+
 const calculateFashion = (arr = []) => {
     //[1,2,1]
     let  objTimes = {}; 
 
+    //Get elements into keys object
     for (let i = 0; i < arr.length; i++) {
         objTimes[arr[i]] = 0
         
     }
 
+    // positions of arr
     for (let i = 0; i < arr.length; i++) {
+        
+        // console.log(arr[i]); 
+
+        //Compare and sum one if is a repped number
         for (let j = 0; j < 10; j++) {    
+            
             if (arr[i] == j) {
-                objTimes[arr[i]] += 1 
+                objTimes[arr[i]] += 1; 
+                // console.log({objTimes});
+
             }
         }
     }
    
-
     console.log({objTimes});
 }
 
